3d570326061b118dbbe72a6da9b7cda2c6319ab1
Matvey Chudakov <matvey159@gmail.com>
2023-09-29 22:29:06 +0300

    Request logging

---
+++ src/main/java/com/github/chMatvey/dronedelivery/core/RequestLoggingFilterConfig.java
@@ -0,0 +1,21 @@
+package com.github.chMatvey.dronedelivery.core;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLoggingFilterConfig {
+
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setIncludeHeaders(true);
+        filter.setIncludeHeaders(false);
+        filter.setMaxPayloadLength(1000);
+
+        return filter;
+    }
+}
