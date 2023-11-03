6cf4565b6363d2712d797cd6eaa12f8785ea2dde
Matvey Chudakov <matvey159@gmail.com>
2023-11-03 23:54:34 +0300

    ktor project

---
+++ src/main/kotlin/com/github/chMatvey/Application.kt
@@ -0,0 +1,13 @@
+package com.github.chMatvey
+
+import com.github.chMatvey.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureRouting()
+}
